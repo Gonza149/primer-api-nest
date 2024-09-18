8dde8651dfd54d31024c9ee642949e0c9c97da29
Gonza149 <elgonza149@gmail.com>
2024-09-18 13:51:57 -0300

    dsalflas

--- src/cats/cat.service.spec.ts
+++ src/cats/cat.service.spec.ts
@@ -1,27 +1,38 @@
 import { CatsService } from "./cats.service";
 import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule, getRepositoryToken } from "@nestjs/typeorm";
 import { Cat } from "./entities/cat.entity";
 import { Breed } from '../breeds/entities/breed.entity';
-import { DataSource } from "typeorm";
-import { CatsModule } from "./cats.module";
+import { DataSource, Repository } from "typeorm";
+
+
+
+const mockCatsService = {};
+const mockBreedService = {};
 
 describe('catsService', ()=>{
     let catsService: CatsService;
+    let catRepository: Repository<Cat>;
+    let breedRepository: Repository<Breed>;
 
     beforeEach(async ()=> {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [TypeOrmModule.forFeature([Cat, Breed]),
-                    CatsModule, DataSource],
+
             providers: [
                 CatsService,
-                {provide: Cat,
-                    useValue: {},
-                }],
+                {provide: getRepositoryToken(Cat),
+                useValue: mockCatsService,
+                },
+                {provide: getRepositoryToken(Breed),
+                useValue: mockBreedService,
+                },
+            ],
         }).compile();
 
         catsService = module.get<CatsService>(CatsService);
-    
+        catRepository = module.get<Repository<Cat>>(getRepositoryToken(Cat));
+        breedRepository = module.get<Repository<Breed>>(getRepositoryToken(Breed));
+
     });
 
     it("Servicios de Cat", () => {
